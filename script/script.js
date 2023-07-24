5393b5a295c6cc4f79ade484926b09e5201a2f28
vmeghna <shrimeghna97@gmail.com>
2023-07-24 11:54:20 +0530

    final

---
+++ script/script.js
@@ -0,0 +1,11 @@
+// const enterButton = document.getElementById("enter");
+// const input = document.getElementById("input");
+// const list = document.querySelector("ul");
+// const tasks = document.querySelector("li");
+// function lengthofInput() {
+//   return input.value.length;
+// }
+// function listElement() {
+//   const li = document.createElement("li");
+//   li.appendChild(document.createTextNode(input.value));
+// }
